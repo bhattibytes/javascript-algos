30169226005a6a6bb402c75060ecf3b8ef02b481
Jason Bhatti <bhattibytes@gmail.com>
2023-08-13 16:20:24 -0400

    Implement: allAnagrams w/recursion

--- allAnagrams.js
+++ allAnagrams.js
@@ -13,5 +13,25 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  // create output array to store anagrams 
+  let anagrams = [];
+  // create inner function to generate anagrams
+  let generateAnagrams = function(ana, str) {
+    // base case: if string is empty, push anagram to output array
+    if (str === '') {
+      anagrams.push(ana);
+    }
+    // iterate through string
+    for (let i = 0; i < str.length; i++) {
+      // recursively call inner function with anagram + current letter removed from string
+      generateAnagrams(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+    }
+  };
+  // call inner function with empty string and input string
+  generateAnagrams('', string);
+  // return output array
+  return anagrams;
 };
+
+let anagrams = allAnagrams('abc');
+console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
