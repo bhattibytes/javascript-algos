ca39143aab42ffe4ab5c92592a323ba795c5d675
Jason Bhatti <bhattibytes@gmail.com>
2023-08-15 11:28:53 -0400

    Implement: rockPaperScissors w/recursion

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -18,8 +18,30 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+const rockPaperScissors = function (num) {
+  // create a result array to store all possible combinations 
+  let results = [];
+  // create a plays array with the 3 possible plays
+  let plays = ['R', 'P', 'S'];
+  // create a recursive function that takes in the 
+  //number of rounds left and the plays so far
+ const recurse = function(roundsLeft, playedSoFar) {
+    // base case: if roundsLeft is 0, push the playsSoFar into the results array
+    if (roundsLeft === 0) {
+      results.push(playedSoFar);
+      return;
+    }
+    // iterate through the plays array
+    for (let i = 0; i < plays.length; i++) {
+      // call the recursive function with roundsLeft - 1 and the playsSoFar + the current play
+      recurse(roundsLeft - 1, playedSoFar + plays[i]);
+    }
+  };
+  // call the recursive function with num and an empty string
+  recurse(num, '');
+  // return the results array
+  return results;
 };
 
+
+console.log(rockPaperScissors(4));
