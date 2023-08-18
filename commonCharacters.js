ed35e2f00bc6613a0d0b7130da9068f3a79e770f
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-08-18 16:37:14 -0400

    Implement: commonCharacters

--- commonCharacters.js
+++ commonCharacters.js
@@ -12,6 +12,24 @@
 
 
 
-var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+const commonCharacters = function(string1, string2) {
+  // create result string to store the characters that are the same 
+  let results = '';
+  // loop over the first string and hold the current letter 
+  for ( let i = 0; i < string1.length; i++ ) {
+    let current = string1[i];
+    // loop over the second string comparing the current letter to each letter of the second string
+    for ( let k = 0; k < string2.length; k++ ) {
+      // if they match add that to the results string and break out of the loop 
+      if (current === string2[k]) {
+        results += current
+        break;
+      }
+    }
+  }
+  // return the results 
+  return results;
 };
+
+let res = commonCharacters('acexivou', 'aegihobu');
+console.log('should equal "aeiou" ', res);
