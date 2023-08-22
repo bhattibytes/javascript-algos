02fb3097fab0f0c65191bf8c4f00b12ed9f7ab50
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-08-22 13:27:58 -0400

    Implement: longestWord

--- longestWord.js
+++ longestWord.js
@@ -2,8 +2,24 @@
 // Write a function that finds the longest word in a string as a given input:
 
 
-function longestWord(str) {
-  // your code goes here
+const longestWord = function(str) {
+  // create a longestWord variable to store the longest word
+  let longestWord = '';
+  // split the str sentence into an array of words
+  let arrayOfWords = str.split(' ');
+  // set the longest word to the first word in the array of words
+  longestWord = arrayOfWords[0];
+  // loop over the rest of the array checking if the length of the current longest is longer
+  for ( let i = 1; i < arrayOfWords.length; i++ ) {
+    //check if the current words is longer than the longest
+    if ( longestWord.length < arrayOfWords[i].length ) {
+      // if it is reassign the arrayOfWords[i] to be the longestWord
+      longestWord = arrayOfWords[i];
+    }
+  }
+  // finally, return the longest word
   return longestWord;
 }
-longestWord("Find a longest word in a string"); // longest will be returned
+
+let res = longestWord("Find a longest word in a string"); // longest will be returned
+console.log("The longest word is: ", res);
