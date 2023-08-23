4d7847cd6e96bed0fbd9760e8958662e891d4ac8
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-08-23 12:28:12 -0400

    Implement: titleCase

--- titleCase.js
+++ titleCase.js
@@ -1,8 +1,19 @@
 // Write a function that will title-case a string as a given input:
 
-function titleCase(str) {
-  // Your code goes here
-  return str;
+const titleCase = function(str) {
+  // create a variable initially an empty string to store the new titleCased string
+  let stringCased = '';
+  // split the string into an array of words
+  let arrayOfWords = str.split(' ');
+  // loop over the array of words 
+  for (let i = 0; i < arrayOfWords.length; i++) {
+  // now capitalize the first letter and add it and rest of the word plus a space to the result string
+  stringCased += arrayOfWords[i][0].toUpperCase() + arrayOfWords[i].slice(1) + ' ';
+  }
+  // return  the new result string
+  return stringCased;
 }
-titleCase("sky is blue"); //Returns: Sky Is Blue
+
+let res = titleCase("sky is blue"); //Returns: Sky Is Blue
+console.log(res)
 
