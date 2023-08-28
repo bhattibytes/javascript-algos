0e30a5f89c5e473d77896f3bc250e64557c86275
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-08-28 09:00:52 -0400

    Implement: splicingArrays

--- splicingArrays.js
+++ splicingArrays.js
@@ -1,9 +1,12 @@
 // Write an algorithm that uses splice
 
 function splicer(arr1, arr2, n) {
-  // Your code goes here
+  // splice arr2 at n delete 0 and spread out arr1 at the index n
+  arr2.splice(n, 0, ...arr1);
+  // return arr2 
   return arr2;
 }
 
-splicer([1, 2, 3], ['a', 'b', 'c'], 2); // Should return: ["a", "b", 1, 2, 3, "c"] 
+let res = splicer([1, 2, 3], ['a', 'b', 'c'], 2); // Should return: ["a", "b", 1, 2, 3, "c"] 
+console.log(res)
 
