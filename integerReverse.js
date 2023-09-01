f5c379aa1cfdaec4708640846b1d3776a04fad2f
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-01 12:11:22 -0400

    Implement: integerReverse

--- integerReverse.js
+++ integerReverse.js
@@ -10,5 +10,18 @@
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  // create a variable to store the reversed number starting at 0 
+  let reversed = 0;
+  // while the number is greater than 0
+  while ( number > 0 ) {
+    // multiply the reversed number by 10 and add the remainder of the number divided by 10
+    reversed = reversed * 10 + number % 10;
+    // divide the number by 10 and round down
+    number = Math.floor( number / 10 )
+  }
+  // return the reversed number
+  return reversed
 }
+
+let res = reverseInteger(12345) 
+console.log(res) // 54321
