f12cf0219540ca83961d90c82b064af864e60813
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-02 15:35:23 -0400

    Implement: largestProductOfThree

--- largestProductOfThree.js
+++ largestProductOfThree.js
@@ -7,7 +7,28 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
-var largestProductOfThree = function(array) {
-  // TODO: everything
+// create helper function to sort array in ascending order
+Array.prototype.sortAscending = function() {
+  this.sort(function(a, b) {
+    return a - b;
+  });
+  return this;
 };
+
+
+const largestProductOfThree = function(array) {
+  // sort array in ascending order with helper function
+  array = array.slice().sortAscending();
+  
+  // assign variables for the largest, second largest, and third largest numbers
+  let largest = array[array.length - 1];
+  let secondLargest = array[array.length - 2];
+  let thirdLargest = array[array.length - 3];
+  
+  // return the largest product of the three largest numbers
+  return largest * Math.max(secondLargest * thirdLargest, array[0] * array[1]);
+};
+
+
+let res = largestProductOfThree([2, 1, 3, 7]);
+console.log('should be 42 ', res);
