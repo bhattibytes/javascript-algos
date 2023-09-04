be8cad85d082e03b41c793605b8011ab36c01f90
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-04 14:54:42 -0400

    Implement: longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -6,5 +6,41 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
+const isPalindrome = function (string) {
+  // iterate through the string
+  for (let i = 0; i < string.length; i++) {
+    // if the current character is not equal to the character at the opposite end of the string
+    if (string[i] !== string[string.length - 1 - i]) {
+      // return false
+      return false;
+    }
+  }
+  // return true
+  return true;
 };
+
+const longestPalindrome = function (string) {
+  // create a variable to store the longest palindrome
+  let longest = '';
+  // iterate through the string
+  for (let i = 0; i < string.length; i++) {
+    // iterate through the string again
+    for (let j = i + 1; j <= string.length; j++) {
+      // create a variable to store the current substring
+      let substring = string.slice(i, j);
+      // if the current substring is a palindrome
+      if (isPalindrome(substring)) {
+        // if the current substring is longer than the longest palindrome
+        if (substring.length > longest.length) {
+          // reassign the longest palindrome to the current substring
+          longest = substring;
+        }
+      }
+    }
+  } 
+  // return the longest palindrome
+  return longest;
+};
+
+let res = longestPalindrome('My dad is a racecar athlete');
+console.log(res);
