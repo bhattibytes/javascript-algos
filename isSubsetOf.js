0dcb6c589fa35c276c07af8c7a66c5ce95080979
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-05 13:18:24 -0400

    Implement: isSubsetOf

--- isSubsetOf.js
+++ isSubsetOf.js
@@ -22,5 +22,54 @@
 */
 
 Array.prototype.isSubsetOf = function (arr) {
-  // your code here
+  // create a variable to hold the count of true values
+  let trueCount = 0;
+  // ensure the input array is unique
+  arr = new Set(arr);
+  // convert the set back to an array
+  arr = Array.from(arr);
+  // ensure the this array is unique
+  let thisSet = new Set(this);
+  // convert the set back to an array
+  thisSet = Array.from(thisSet);
+  // loop over the input array 
+  for (let i = 0; i < arr.length; i++) {
+  // check to see if the arr contains objects or arrays as inner elements
+    if (typeof arr[i] === 'object') {
+      // if so, loop over the inner elements
+      for (let j = 0; j < arr[i].length; j++) {
+        // check to see if the inner elements are equal to the current element
+        if (arr[i][j] === thisSet[i]) {
+          // if so, increase the true count by 1 
+          trueCount++;
+        }
+      }
+    // otherwise if the inner elements are not objects or arrays
+    } else {
+      // loop over the thisSet array
+      for (let k = 0; k < thisSet.length; k++) {
+        // check to see if the current element is equal to the current element in the thisSet array
+        if (arr[i] === thisSet[k]) {
+          // if so, increase the true count by 1
+          trueCount++;
+        }
+      }
+    }
+  }
+  // check to see if the true count is equal to the length of the thisSet array
+  if ( trueCount === thisSet.length ) {
+    // if so, return true
+    return true;
+  } else {
+    // otherwise, return false
+    return false;
+  }
 };
+
+let a = ['commit','push']
+let res = a.isSubsetOf(['commit','rebase','push','blame']) // true
+console.log(res);
+
+let b = ['merge','reset','reset']
+let res2 = b.isSubsetOf(['reset','merge','add','commit']) // true 
+console.log(res2);
