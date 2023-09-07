1d37fbcde68e510058e5de5f30ada477a518f1e6
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-07 12:20:05 -0400

    Implement: powerSet

--- powerSet.js
+++ powerSet.js
@@ -17,5 +17,35 @@
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str) {
+const powerSet = function(str) {
+  // create a storage variable assign to an array with an empty string 
+  let store = [''];
+  // create helper function that takes in a prefix and a string 
+  const addToStore = function (prefix, str) {
+    // base case: if the string is empty, return an array with an empty string
+    if ( !str.length ) {
+      return [''];
+    }
+    // recursive case:
+    for ( let i = 0; i < str.length; i++ ) {
+      // create a variable that is the prefix plus the current letter
+      let pre = prefix + str[i];
+      // if the storage array does not include the prefix, push the prefix into the storage array
+      if ( !store.includes(pre) ) {
+        store.push(pre);
+      }
+      // call the helper function with the prefix and the string minus the current letter
+      addToStore(pre, str.slice(i+1));
+    }
+  }
+  // call the helper function with an empty string and the input string
+  addToStore('', str);
+  // return the storage array
+  return store;
 };
+
+let res1 = powerSet("abc")
+console.log("should be [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]: ", res1);
+
+let res2 = powerSet("jump")
+console.log('should be ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]', res2);
