65cdb47a5ff12dabb491e8b9d9b5af7e5f6f88d4
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-08 16:53:59 -0400

    Implement: numberToEnglish

--- numberToEnglish.js
+++ numberToEnglish.js
@@ -54,5 +54,61 @@ var numbersToPlace = {
 };
 
 Number.prototype.toEnglish = function () {
-  // return my value as english words
+  // create an inner recursive function that takes in a number and returns a string
+  let innerRecurse = (num) => {
+    // create varaibles for place, placeValueNum, toBeRecrused, and results
+    let place;
+    let placeValueNum;
+    let toBeRecrused;
+    let results = '';
+    // if the number is in the numbersToWords object
+    if (numbersToWords[num]) {
+      // set results to the value of the number in the numbersToWords object
+      results = numbersToWords[num];
+      // else if the number is less than 100
+      } else if (num < 100) { 
+        // set placeValueNum to the number rounded down to the nearest 10
+        placeValueNum = Math.floor(num / 10) * 10;
+        // set toBeRecrused to the number modulo 10
+        toBeRecrused = num % 10;
+        // set results to the value of the number in the numbersToWords object
+        results = numbersToWords[placeValueNum] + '-' + numbersToWords[toBeRecrused];
+      // else if the number is less than 1000
+      } else {
+        if (num < 1000) {
+        // set place to 100
+        place = 100;
+        } else {
+        // otherwise set place to 1000
+        place = 1000;
+          // while place * 1000 is less than or equal to the number
+          while (place * 1000 <= num) {
+          // multiply place by 1000
+          place *= 1000;
+          }
+        }
+      // set placeValueNum to the number rounded down to the nearest place
+      placeValueNum = Math.floor(num / place);
+      // set toBeRecrused to the number modulo place
+      toBeRecrused = num % place;
+      // set results to the value of the number in the numbersToWords object
+      results = innerRecurse(placeValueNum) + ' ' + numbersToPlace[place];
+      let restOfNumber;
+      // set restOfNumber to the value of the number in the numbersToWords object
+      restOfNumber = innerRecurse(toBeRecrused);
+      // if toBeRecrused is not zero
+      if (restOfNumber !== 'zero') {
+        // set results to results plus the value of the number in the numbersToWords object
+        results += ' ' + restOfNumber;
+      }
+    }
+    // return results
+    return results;
+  }
+  // return the result of calling the inner recursive function on this
+  return innerRecurse(this);
 };
+
+console.log((7).toEnglish()) // seven
+console.log((575).toEnglish()); // > "five hundred seventy-five"
+console.log((78193512).toEnglish()); // > "seventy-eight million one hundred ninety-three thousand five hundred twelve"
