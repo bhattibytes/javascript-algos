9af7a517607893dd878cc8a38d794338a054eaa3
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-24 14:52:38 -0400

    Implement: binarySearchArray

--- binarySearchArray.js
+++ binarySearchArray.js
@@ -11,4 +11,29 @@
  */
 
 var binarySearch = function (array, target) {
+  // create min, max, and mid variables as pointers to the array
+  var min = 0;
+  var max = array.length - 1;
+  var mid = Math.floor((min + max) / 2);
+  // while the min is less than or equal to the max
+  while (min <= max) {
+    // if the mid is equal to the target, return the mid
+    if (array[mid] === target) {
+      return mid;
+      // otherwise, if the mid is less than the target, set the min to mid + 1
+    } else if (array[mid] < target) {
+      min = mid + 1;
+    } else {
+      // otherwise, set the max to mid - 1
+      max = mid - 1;
+    }
+    // reassign the mid to the new mid
+    mid = Math.floor((min + max) / 2);
+  }
+  // return null if the target is not found
+  return null;
 };
+
+
+console.log(binarySearch([1, 2, 3, 4, 5], 4), 'should be 3');
+console.log(binarySearch([1, 2, 3, 4, 5], 8), 'should be null');
