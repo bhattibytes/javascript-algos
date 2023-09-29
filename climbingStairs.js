ee597f6953bb24aa8bb4d622bbfa3d4807e7194e
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-09-29 12:04:04 -0400

    Implement: climbingStairs (dynamic programming) refactor

--- climbingStairs.js
+++ climbingStairs.js
@@ -49,12 +49,22 @@
  * @return {number}
  */
 const climbStairs = function(n) {
-  // base cases
-  if (n === 1) return 1;
-  if (n === 2) return 2;
+  // // base cases
+  // if (n === 1) return 1;
+  // if (n === 2) return 2;
 
-  // recursive case
-  return climbStairs(n-1) + climbStairs(n-2);
+  // // recursive case
+  // return climbStairs(n-1) + climbStairs(n-2);
+  
+  // refactored to use dynamic programming
+  const dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+
 };
 
 console.log("Should be 2", climbStairs(2));
