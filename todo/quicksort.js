fe9ea68913fe47da5fb97d29077025d89bfe8404
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-10-28 23:46:59 -0400

    Implement: quicksort

--- todo/quicksort.js
+++ todo/quicksort.js
@@ -12,4 +12,32 @@
 
 
 var quicksort = function(array) {
+  // pick a value in the array as a pivot
+  var pivot = array[0];
+  // partition all the elements of the array into two arrays, based on
+  // if they are larger or smaller than the pivot
+  var left = [];
+  var right = [];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+  // recursively sort the two halves
+  if (left.length > 1) {
+    left = quicksort(left);
+  }
+  if (right.length > 1) {
+    right = quicksort(right);
+  }
+  // combine the two arrays and the pivot into a sorted array
+  return left.concat(pivot, right);
 };
+
+
+console.log(quicksort([5, 4, 3, 2, 1])); // [1, 2, 3, 4, 5]
+console.log(quicksort([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(quicksort([1, 2, 3, 5, 4])); // [1, 2, 3, 4, 5]
+console.log(quicksort([1, 2, 3, 5, 4, 6])); // [1, 2, 3, 4, 5, 6]
