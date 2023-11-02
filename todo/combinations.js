f131c0e5a8ea93c643fcb081019dedc7243d69b3
Jason Bhatti <bhattibytes@gmail.com>
2023-11-02 16:39:33 -0500

    Implement: combinations

--- todo/combinations.js
+++ todo/combinations.js
@@ -29,5 +29,29 @@
  * @return {number[][]}
  */
 const combine = function(n, k) {
-    
+    // create a result array
+    const result = [];
+    // create a helper function to recurse through the numbers
+    const helper = (current, start) => {
+        // if the current array is the length of k, push it into the result array
+        if (current.length === k) {
+            result.push(current);
+            return;
+        }
+        // loop through the numbers from start to n
+        for (let i = start; i <= n; i++) {
+            // call the helper function with the current array plus the current number and the next number as the start
+            helper([...current, i], i + 1);
+        }
+    }
+    // call the helper function with an empty array and 1 as the start
+    helper([], 1);
+    // return the result array
+    return result;
 };
+
+console.log(combine(4, 2)); // [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+console.log(combine(1, 1)); // [[1]]
+console.log(combine(5, 3)); // [[1,2,3],[1,2,4],[1,2,5],[1,3,4],[1,3,5],[1,4,5],[2,3,4],[2,3,5],[2,4,5],[3,4,5]]
+console.log(combine(3, 2)); // [[1,2],[1,3],[2,3]]
+console.log(combine(3, 1)); // [[1],[2],[3]]
