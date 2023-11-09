0f40baccb8e365f4d9c7c2818acd2887464faa13
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-11-09 17:23:34 -0500

    Implement: longestRun

--- completed/longestRun.js
+++ completed/longestRun.js
@@ -13,8 +13,28 @@
  * inputs well.
  */
 
-var longestRun = function (string) {
-  // TODO: Your code here!
+const longestRun = function (string) {
+  if (string.length === 0) return null;
+  let longest = [0, 0];
+  let current = [0, 0];
+  let currentChar = string[0];
+  let currentCount = 0;
+  let longestCount = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === currentChar) {
+      currentCount++;
+      current[1] = i;
+    } else {
+      currentChar = string[i];
+      currentCount = 1;
+      current = [i, i];
+    }
+    if (currentCount > longestCount) {
+      longestCount = currentCount;
+      longest = current;
+    }
+  }
+  return longest;
 };
 
 // If you need a random string generator, use this!
@@ -29,3 +49,8 @@ var randomString = function (len) {
 
   return text;
 };
+
+console.log(longestRun('abbbcc')); // [1, 3]
+console.log(longestRun('aabbc')); // [0, 1]
+console.log(longestRun('abcd')); // [0, 0]
+console.log(longestRun('')); // null
