92b496ecc81718100f80308dd26c4e4e2fb14a1c
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-11-11 19:01:48 -0500

    Implement: asyncMap

--- completed/asyncMap.js
+++ completed/asyncMap.js
@@ -39,4 +39,38 @@
 
 
 var asyncMap = function(tasks, callback) {
+  var results = [];
+  var count = 0;
+
+  for (var i = 0; i < tasks.length; i++) {
+    (function(i) {
+      tasks[i](function(val) {
+        results[i] = val;
+        count++;
+        if (count === tasks.length) {
+          callback(results);
+        }
+      });
+    })(i);
+  }
+
 };
+
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+  });
+  
