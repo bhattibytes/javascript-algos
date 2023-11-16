18242ae8dd9f620ae79e5f52fb61b2dfcf66e15e
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2023-11-16 18:12:18 -0500

    Implement: maxDepthTree

--- completed/maxDepthTree.js
+++ completed/maxDepthTree.js
@@ -33,5 +33,34 @@
  * @return {number}
  */
 const maxDepth = function(root) {
-    
+  if (!root) {
+    return 0;
+  }
+
+  let queue = [root];
+  let depth = 0;
+
+  while (queue.length) {
+    let size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    depth++;
+  }
+
+  return depth;
 };
+
+console.log(maxDepth([3,9,20,null,null,15,7])); // => 3
+console.log(maxDepth([1,null,2])); // => 2
+console.log(maxDepth([])); // => 0
